8eede599cee5fa2e29e9caae92222236e3b00730
anis-zariat <anis.zariat@littlewolf.tech>
2026-01-23 11:47:54 +0100

    A self-attention class using PyTorch’s Linear layers

--- attention-mechanism-implementation.py
+++ attention-mechanism-implementation.py
@@ -1,5 +1,6 @@
 import torch
 from selfAttentionClass import SelfAttention_v1
+from selfAttentionClass import SelfAttention_v2
 
 torch.manual_seed(123)
 
@@ -16,3 +17,8 @@ d_in = inputs.shape[1]  # The input embedding size, d=3
 d_out = 2  # The output embedding size, d_out=2
 sa_v1 = SelfAttention_v1(d_in, d_out)
 print(sa_v1(inputs))
+
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
