4158511edee82dc84a445201ec362f39c3bdcd65
anis-zariat <anis.zariat@littlewolf.tech>
2026-01-26 16:28:54 +0100

    implement layer normalization to improve the stability and efficiency of neural network training

--- GPTModel/gpt.py
+++ GPTModel/gpt.py
@@ -1,6 +1,7 @@
 import tiktoken
 import torch
 from DummyGPTModelClass import DummyGPTModel
+import torch.nn as nn
 
 # The configuration of the small GPT-2 model via the following Python dictionary:
 GPT_CONFIG_124M = {
@@ -24,8 +25,23 @@ batch = torch.stack(batch, dim=0)
 print(batch)
 
 # we initialize a new 124-million-parameter DummyGPTModel instance and feed it the tokenized batch:
+""" 
 torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch)
 print("Output shape:", logits.shape)
 print(logits)
+"""
+# implement layer normalization to improve the stability and efficiency of neural network training
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+# 1 Creates two training examples with five dimensions (features) each
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+# let’s examine the mean and variance:
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
