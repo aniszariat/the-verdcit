9a18737cba09f71774669b6a0da1777f35c34239
anis-zariat <anis.zariat@littlewolf.tech>
2026-01-26 15:55:58 +0100

    Coding an LLM architecture

---
+++ GPTModel/gpt.py
@@ -0,0 +1,24 @@
+import tiktoken
+import torch
+
+# The configuration of the small GPT-2 model via the following Python dictionary:
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 1024,  # Context length
+    "emb_dim": 768,  # Embedding dimension
+    "n_heads": 12,  # Number of attention heads
+    "n_layers": 12,  # Number of layers
+    "drop_rate": 0.1,  # Dropout rate
+    "qkv_bias": False,  # Query-Key-Value bias
+}
+
+# we tokenize a batch consisting of two text inputs for the GPT model using the tiktoken tokenizer
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
