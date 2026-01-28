1f0eeeaaa3942e399809e4daaf0bf915a3a0c3e2
anis-zariat <anis.zariat@littlewolf.tech>
2026-01-28 14:08:54 +0100

    general refactoring

---
+++ chapter04_GPTModel/transformerBlock.py
@@ -0,0 +1,36 @@
+# The transformer block component of GPT
+from chapter03 import MultiHeadAttention
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"],
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # 1 Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        # 2 Add the original input back
+        x = x + shortcut
+        # 3 Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        # 4 Adds the original input back
+        x = x + shortcut
+        return x
