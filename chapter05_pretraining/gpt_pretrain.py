9eb76e542f5ce35efb5425b323bdbf828b82492a
anis-zariat <anis.zariat@littlewolf.tech>
2026-04-06 18:18:53 +0100

    loss evaluation

--- chapter05_pretraining/gpt_pretrain.py
+++ chapter05_pretraining/gpt_pretrain.py
@@ -27,8 +27,17 @@ def main():
         context_size=GPT_CONFIG_124M["context_length"]
     )
 
-    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
-    
+    # print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+    inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+    targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+    with torch.no_grad():
+        logits = model(inputs)
+
+    probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+    print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size)
 
 if __name__=="__main__":
     main()
